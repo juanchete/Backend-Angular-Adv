a71ea8de3599296964866dfe7b21a84a4825d6fa
juanchete <juanlopezlmg@gmail.com>
2020-08-04 21:18:46 -0400

    Google Sign In

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const{response} = require('express');
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
+const {googleVerify} = require('../helpers/google-verify');
+const usuario = require('../models/usuario');
 
 const login = async ( req, res = response) => {
 
@@ -47,6 +49,56 @@ const login = async ( req, res = response) => {
 
 }
 
+const googleSignIn = async (req, res = response) =>{
+
+    const googleToken = req.body.token;
+
+    try {
+
+        const {name, email, picture} = await googleVerify( googleToken);
+
+        const usuarioDB = await Usuario.findOne({email})
+
+        let usuario;
+
+        if(!usuarioDB){
+             usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '00',
+                img: picture,
+                google: true
+            })
+        }else {
+            usuario=usuarioDB
+            usuario.google = true
+        }
+
+        await usuario.save()
+
+         //Generar el JWT
+         const token = await generarJWT(usuario.id)
+
+        res.json({
+            ok:true,
+            token
+        })
+        
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(401).json({
+            ok:false,
+            msg:'El Token no es valido'
+        })
+        
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
